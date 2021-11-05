4399ef6a7219e7eef348bceefdc5abfa3d9981c8
dweiskopf <danielaaronw@gmail.com>
2021-11-05 09:13:26 -0700

    setting up demo

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
-# provider "aws" {
-#   region = var.region
-# }
+provider "aws" {
+  region = var.region
+}
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  owners = ["099720109477"] # Canonical
+}
 
-# resource "aws_instance" "ubuntu" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = var.instance_type
-#   associate_public_ip_address = true
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  associate_public_ip_address = true
 
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
+  tags = {
+    Name = var.instance_name
+  }
+}
 
-# resource "aws_s3_bucket" "b" {
-#   bucket = var.bucket_name
-#   acl    = var.acl
+resource "aws_s3_bucket" "b" {
+  bucket = var.bucket_name
+  acl    = var.acl
 
-#   tags = {
-#     Name        = var.bucket_name
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = var.bucket_name
+    Environment = "Dev"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
-}
+# output "instance_ami" {
+#   value = aws_instance.ubuntu.ami
+# }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
-}
+# output "instance_arn" {
+#   value = aws_instance.ubuntu.arn
+# }
 
-output "bucket" {
-  value = aws_s3_bucket.b
-}
+# output "bucket" {
+#   value = aws_s3_bucket.b
+# }
